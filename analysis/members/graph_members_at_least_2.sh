7a277d484fab9ffdc15f0db9a95f59a4e671ab1e
ramonfontes <ramonreisfontes@gmail.com>
2015-03-19 11:44:15 -0300

    adding members

--- analysis/members/graph_members_at_least_2.sh
+++ analysis/members/graph_members_at_least_2.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import zipfile, cStringIO
 import networkx as nx
 import matplotlib.pyplot as plt
 import math
@@ -8,15 +9,10 @@ import glob
 from networkx import *
 from operator import itemgetter, attrgetter, methodcaller
 
-#Read this file
 file_name="report_graph_members.txt"
-#At current path
 myPath="."
-#Save the file
 files=glob.glob1(myPath,file_name)
-#output file name
-outputfile="members_sorted.txt"
-#remove the output file if it exists
+outputfile="members_sorted_at_least_2_ptts.txt"
 os.system('rm '+ outputfile)
 
 o=0;
@@ -26,27 +22,28 @@ node = [[0 for x in xrange(2)] for x in xrange(1000)]
 e1=open(file_name, 'r')
 print "Processing File..."+file_name
 
+#Declaracao de variaveis
 i=0;j=0;
 
-#Read Path files
+#Laco para leitura dos arquivos de Path
 for line in e1:
     j+=1
-    #Read the columns
+    #Laco para leitura das colunas
     while i <= 1:
-        #If first column
+        #Se primeira coluna
         if (i==0):
-            #Save the ASN ID
+            #Armazena o AS ID
             node[o][0]=str(line.strip().split(' ')[0])
-        #If second column
+        #Se segunda coluna
         elif(i==1):
             node[o][1]=str(line.strip().split(' ')[1])
             #os.system('echo '+str(node[o][0])+" "+str(node[o][1])+'  >> '+file_name)
             o+=1
-        #Go to next column
+        #Incrementa a leitura para uma nova coluna
         i+=1
-    #Go to column 0
+    #Inicia uma nova linha na coluna 0
     i=0
-#---------------------------------------------------------------------
+
 member=""
 members=["0"]
 ptt=""
@@ -57,8 +54,8 @@ for line in range(0,j):
         if(str(node[line_][1])==member and member not in members):
             members.append(member)
 
-#remove member 0
 members.pop(0)
+#print sorted(members)
 #----------------------------------------------------------------------
 membersptt=[]
 membersorted=[]
@@ -71,7 +68,8 @@ for line in range(0,len(members)):
 
 membersorted = sorted(membersptt)
 
-#Save the list of sorted members
 for line in range(0,len(membersorted)):
-    os.system('echo ASN:'+membersorted[line]+'  >> '+"members_sorted.txt")
-os.system('echo TOTAL ASNs:'+str(j)+' >> '+outputfile)
+    os.system('echo ASN:'+membersorted[line]+'  >> '+outputfile)
+os.system('echo TOTAL ASNs:'+str(len(membersorted))+' >> '+outputfile)
+
+    #print membersorted[line]

c73a3bb4ac9455167f2da1023abd1a7b9f504fc2
shbbrito <shbbrito@gmail.com>
2015-06-14 17:51:36 -0300

    Update PTTbr-RAW.sh

--- raw_data/PTTbr-RAW.sh
+++ raw_data/PTTbr-RAW.sh
@@ -14,17 +14,11 @@
 
 all_ixp="bel ba cas ce cgb cpv cxj df gyn laj lda mao mg mgf nat pe pr rj rs sc sca sjc sjp sp vix"
 pub_ixp="bel ce cpv df gyn lda mg pe pr rj rs sjc sp vix"
-dbg_ixp="bel sjc"
-
-###--- problems due to timeout on telnet: CE, CPV, DF, GYN, MG, PE, RS, VIX
+dbg_ixp="bel"
 
 for code in ${pub_ixp}; do
-  (echo "term l 0"; sleep 1; echo "show ip bgp"               ; sleep 8m; echo "exit";) | telnet lg.${code}.ptt.br | tee lg.${code}.ptt.br-BGP-IPv4.txt
+  (echo "term l 0"; sleep 1; echo "show ip bgp"               ; sleep 7m; echo "exit";) | telnet lg.${code}.ptt.br | tee lg.${code}.ptt.br-BGP-IPv4.txt
   (echo "term l 0"; sleep 1; echo "show ipv6 bgp"             ; sleep 5m; echo "exit";) | telnet lg.${code}.ptt.br | tee lg.${code}.ptt.br-BGP-IPv6.txt
   (echo "term l 0"; sleep 1; echo "show ip bgp paths"         ; sleep 2m; echo "exit";) | telnet lg.${code}.ptt.br | tee lg.${code}.ptt.br-BGP-Paths.txt
   (echo "term l 0"; sleep 1; echo "show ip bgp community-info"; sleep 1m; echo "exit";) | telnet lg.${code}.ptt.br | tee lg.${code}.ptt.br-BGP-Community.txt
 done
-
-
-
-
